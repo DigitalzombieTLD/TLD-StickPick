bad260ba77a9e2bf4d01ff69320f0b4bea7b5050
DigitalzombieTLD <christian.gendner@gmx.net>
2021-12-13 20:34:29 +0100

    Add project files.

---
+++ StickPickSettings.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Reflection;
+using UnityEngine;
+using ModSettings;
+
+namespace StickPick
+{
+    internal class StickPickSettingsMain : JsonModSettings
+    {    
+        [Section("Keybind")]
+
+		[Name("Pick up sticks")]
+		[Description("pickSticks")]
+		public UnityEngine.KeyCode buttonPickSticks = KeyCode.LeftAlt;
+
+		[Section("Other settings")]
+
+		[Name("What to pick")]
+		[Description("Choose between stick, stones or sticks & stones")]
+		[Choice("Sticks", "Stones", "Sticks and stones")]
+		public int pickupChoice = 0;
+
+		[Name("Pickup radius")]
+		[Description("Choose the radius (sphere) around the player which gets searched for objects")]
+		[Slider(0f, 8f)]
+		public float pickupRadius = 2.0f;
+
+		[Name("Pickup calorie cost")]
+		[Description("Calorie penalty for picking up a single stick/stone (multiplied if more than one items gets picked up)")]
+		[Slider(0, 25)]
+		public int calorieCost = 10;
+
+		protected override void OnConfirm()
+        {
+            base.OnConfirm();
+        }
+    }
+
+    internal static class Settings
+    {
+        public static StickPickSettingsMain options;
+
+        public static void OnLoad()
+        {
+            options = new StickPickSettingsMain();
+            options.AddToModSettings("StickPick Settings");
+        }
+    }
+}
---
+++ Stickpick.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using MelonLoader;
+using Harmony;
+using UnityEngine;
+using System.Reflection;
+using System.Xml.XPath;
+using System.Globalization;
+using UnhollowerRuntimeLib;
+using ModSettings;
+using System.Collections;
+
+namespace StickPick
+{
+    public class StickPick_Main : MelonMod
+    {
+		public static GameObject playerObject;
+		public static int layerMask = 0;
+
+		public static int stoneCounter = 0, stickCounter = 0;
+
+		public override void OnApplicationStart()
+        {
+			layerMask |= 1 << 17; // gear layer		
+			StickPick.Settings.OnLoad();
+		}
+
+		public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+		{
+			
+		}
+
+		public override void OnUpdate()
+		{
+			if (InputManager.GetKeyDown(InputManager.m_CurrentContext, Settings.options.buttonPickSticks)) 
+			{				
+				RaycastHit[] sphereTargethit;
+				sphereTargethit = Physics.SphereCastAll(GameManager.GetVpFPSPlayer().transform.position, Settings.options.pickupRadius, GameManager.GetVpFPSPlayer().transform.TransformDirection(Vector3.down), GameManager.GetVpFPSPlayer().Controller.m_Controller.height * 0.8f, layerMask);
+			
+				foreach (RaycastHit foo in sphereTargethit)
+				{
+					GearItem foundItem = foo.transform.gameObject.GetComponent<GearItem>();
+
+					if(foundItem && foundItem.enabled)
+					{	
+						if (foundItem.m_GearName.Contains("GEAR_Stick") && !foundItem.m_InPlayerInventory && (Settings.options.pickupChoice == 0 || Settings.options.pickupChoice == 2))
+						{
+							GameAudioManager.PlaySound(foundItem.m_PutBackAudio, InterfaceManager.GetSoundEmitter());
+							GameManager.GetPlayerManagerComponent().ProcessPickupItemInteraction(foundItem, false, true);
+							stickCounter++;
+						}
+
+						if (foundItem.m_GearName.Contains("GEAR_Stone") && !foundItem.m_InPlayerInventory && (Settings.options.pickupChoice == 1 || Settings.options.pickupChoice == 2))
+						{
+							GameAudioManager.PlaySound(foundItem.m_PutBackAudio, InterfaceManager.GetSoundEmitter());
+							GameManager.GetPlayerManagerComponent().ProcessPickupItemInteraction(foundItem, false, true);
+							stoneCounter++;
+						}					
+					}
+				}
+
+				int calorieCost = 0;
+				int itemCount = stoneCounter + stoneCounter;
+
+				if( itemCount!=0 && Settings.options.calorieCost != 0)
+				{
+					calorieCost = calorieCost * Settings.options.calorieCost;
+					GameManager.GetHungerComponent().RemoveReserveCalories(calorieCost);
+				}						
+					
+				MelonLogger.Msg("Picked up " + stickCounter + " sticks and " + stoneCounter + " stones in radius of " + Settings.options.pickupRadius + " meter and spent " + (Settings.options.calorieCost * sphereTargethit.Length) + " calories!");
+				stoneCounter = 0;
+				stickCounter = 0;				
+			}
+		}
+	}
+}
